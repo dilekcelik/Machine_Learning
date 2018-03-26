7a396e469175450a691d04914add1f64e1969af0
Dilek Celik <dilekcell40@gmail.com>
2018-03-26 17:55:03 +0100

    Linear Regression Example Works
    Linear Regression Example Works

---
+++ LinearRegression_Ex1.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 26 15:47:00 2018
+@author: dilekcelik
+
+LINEER REGRESSION
+"""
+
+import numpy as np # mathmatical calculations
+import pandas as pd # data
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt #for visualisations
+
+#read data
+data = pd.read_csv("/Users/dilekcelik/Desktop/Machine_Learning/linear.csv")
+print(data) #print data
+
+x = data["metrekare"]
+y = data["fiyat"]
+x = pd.DataFrame.as_matrix(x) #Turn it into Pandas Matrix
+y = pd.DataFrame.as_matrix(y) 
+
+print(x)
+print(y) #see what we have done
+
+plt.scatter(x,y) #To see what we have done in 2D graph
+
+#Polyfit - Numpy bizim icin grafige oturtuyor cizgimizi
+m,b = np.polyfit(x,y,1)
+a = np.arange(150) #Denklem hzir, a'nin araligini ayarlayalim
+
+plt.scatter(x,y) #Scatter ile nokt cizimleri yapiyoruz
+plt.plot(m*a+b) #cizgiyi cizdik
+
+
+#TEST EDELIM:
+MeterSqure = int(input("Kac metrekare? :"))
+Guess = m*MeterSqure+b
+print(Guess)
+plt.scatter(MeterSqure, Guess, c="pink", marker=">")
+plt.show()
+print("y=",m,"x+",b)
---
+++ LinearRegression_sklearn_Ex1.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 26 15:47:00 2018
+@author: dilekcelik
+
+LINEER REGRESSION
+"""
+
+import numpy as np # mathmatical calculations
+import pandas as pd # data
+from sklearn.linear_model import LinearRegression as lr
+import matplotlib.pyplot as plt #for visualisations
+
+#read data with pandas
+data = pd.read_csv("/Users/dilekcelik/Desktop/Machine_Learning/linear.csv")
+
+x = data["metrekare"]
+y = data["fiyat"]
+print(data)
+
+
+x = x.reshape(99,1) #99X1 lik matrisler oldugunu belirttik
+y = y.reshape(99,1)
+print(data)
+
+lineerregresyon = lr() #Define lineer regression
+lineerregresyon.fit(x,y) # fit x and y, x ve y'yi oturttuk
+lineerregresyon.predict(x)
+#dogrumuzun formulu = mx+b, m= egim, b = kesistigi nokta
+
+#print('Egim (m): /n', lineerregresyon.coef_)
+#print('Y de kesistigi yer: ', lineerregresyon.intercept_)
+
+m = lineerregresyon.coef_
+b = lineerregresyon.intercept_
+#print("Denklem: ")
+#print("y=",m,"x+",b)
+
+#SIMDI NASIL GOZUKTUGUNE BAKALIM, CIZDIRECEGIZ
+a = np.arange(150)
+a=a.reshape(150,1) #2D Matrix yapmam gerekti, burasi yokken hata verdi
+
+plt.scatter(x,y)
+plt.plot(a,m*a+b)
+plt.show()
